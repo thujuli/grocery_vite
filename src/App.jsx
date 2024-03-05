bed1145df8cf9e1ec7481b35ddcd58a4a4668317
AshecOne <77070197+AshecOne@users.noreply.github.com>
2024-03-05 13:57:02 +0700

    feat: Component button oval (#5)
    * feat: Component button oval
    
    * feat: Component button oval

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import ButtonOval from "./components/ButtonOval";
+
 
 function App() {
   return (
     <>
       <div>
         <h1>Grocery Project Landing Page</h1>
+        <ButtonOval >See All</ButtonOval>
       </div>
     </>
   );
