307c7b6be20efd9951e1f46d98e3ad5b774dbfbe
ronaldj220 <65749850+ronaldj220@users.noreply.github.com>
2024-03-05 15:50:51 +0700

    fix: container width (#8)

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,6 @@
 function Container(props) {
   const { children } = props;
-  return <div className="w-[1440px] px-[50px] mx-auto">{children}</div>;
+  return <div className="max-w-[1440px] px-[50px] mx-auto">{children}</div>;
 }
 
 export default Container;
