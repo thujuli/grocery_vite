5a1654510f9eb3325b80376bac820d4150bcc5c7
AshecOne <haseyong123@gmail.com>
2024-03-05 13:44:13 +0700

    feat: Component button oval

---
+++ src/components/ButtonOval.jsx
@@ -0,0 +1,17 @@
+function ButtonOval(props) {
+    const {mode} = props
+    const buttonStyle = `h-10 px-8 rounded-3xl font-normal align-middle shadow-md text-xs ${
+        mode === "dark" ? 'bg-[#2e4859] text-white' : 'bg-white text-black'
+      }`;
+    
+      return (
+        <button
+          className={buttonStyle}
+          style={{ fontFamily: "Roboto" }}
+        >
+          {props.children}
+        </button>
+  )
+}
+
+export default ButtonOval
