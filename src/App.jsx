18973916e41cbc754d928777b1a8b3240910272a
I Putu Julianta <95893131+thujuli@users.noreply.github.com>
2024-03-05 15:36:51 +0700

    feat: Section jumbotron (#7)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import Jumbotron from "./views/Jumbotron";
 
 function App() {
   return (
     <>
-      <div>
-        <h1>Grocery Project Landing Page</h1>
-      </div>
+      <Jumbotron />
     </>
   );
 }
