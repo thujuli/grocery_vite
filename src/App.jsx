f0063ef09538f865e52cd31527b0c36315f02a74
AshecOne <77070197+AshecOne@users.noreply.github.com>
2024-03-06 16:58:26 +0700

    feat: Promo card component and section for promo (#20)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Categories from "./views/Categories";
 import Jumbotron from "./views/Jumbotron";
+import Promo from "./views/Promo";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Jumbotron />
       <Categories />
+      <Promo/>
       <Footer />
     </>
   );
