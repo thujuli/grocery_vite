895cdf8fa0bf2ba199958debbaa83ffbb7b614f9
thujuli <thujuli037@gmail.com>
2024-03-09 01:26:21 +0700

    refactor: remove padding on mobile device

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,8 @@
 function Container(props) {
   const { children } = props;
-  return <div className="max-w-[1440px] px-[50px] mx-auto">{children}</div>;
+  return (
+    <div className="max-w-[1440px] px-0 md:px-[50px] mx-auto">{children}</div>
+  );
 }
 
 export default Container;
