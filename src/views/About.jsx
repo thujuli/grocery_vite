8bcc93467935fa161bb51ba3f7e044c39f6db0a9
AshecOne <haseyong123@gmail.com>
2024-03-09 14:41:47 +0700

    refactor: responsive of about and promo section but cardpromo is fixed

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -6,36 +6,36 @@ function About() {
   return (
     <section id="about">
       <Container>
-        <div
-          className="flex items-center min-h-screen w-full px-[50px] bg-cover bg-center"
-        >
-          <div className="w-1/2">
-            <img src={imgAbout} alt="Image About" />
+        <div className="flex flex-col lg:flex-row mb-8 lg:mb-0 items-center min-h-screen w-full px-4 lg:px-[50px] bg-cover bg-center">
+          <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+            <img src={imgAbout} alt="Image About" className="mx-auto lg:mx-0" />
           </div>
-          
-          <div className="w-1/2">
+
+          <div className="lg:w-1/2 w-full">
             <h2
               style={{ fontFamily: "Roboto" }}
-              className="text-lg text-[#3cb815] font-medium"
+              className="text-lg text-[#3cb815] font-medium text-center lg:text-left"
             >
               ABOUT US
             </h2>
-            <h1 className="mb-6 text-5xl font-bold leading-tight text-[#1a2428]">
+            <h1 className="mb-4 text-3xl lg:text-5xl font-bold leading-tight text-[#1a2428] text-left">
               We Believe In Working With Accredited Farmers
             </h1>
-            <p className="mb-4 text-base text-[#868990]">
-            Organic Foods And Cafe Is A Family Run Company That Runs Organic
+            <p className="mb-2 text-sm lg:text-base text-[#868990] text-left">
+              Organic Foods And Cafe Is A Family Run Company That Runs Organic
             </p>
-            <p className="mb-4 text-base text-[#868990]">
-            Super Markets And Cafes Selling Fresh Organic And Biodynamic Food,
+            <p className="mb-2 text-sm lg:text-base text-[#868990] text-left">
+              Super Markets And Cafes Selling Fresh Organic And Biodynamic Food,
             </p>
-            <p className="mb-4 text-base text-[#868990]">
-            Suppliers, Skincare, Cosmetics, Baby Items And Household Cleaning
+            <p className="mb-2 text-sm lg:text-base text-[#868990] text-left">
+              Suppliers, Skincare, Cosmetics, Baby Items And Household Cleaning
             </p>
-            <p className="mb-10 text-base text-[#868990]">
-            Products. We Have 5 Shops In Dubai - On Sheikh Zayed Road.
+            <p className="mb-6 text-sm lg:text-base text-[#868990] text-left">
+              Products. We Have 5 Shops In Dubai - On Sheikh Zayed Road.
             </p>
-            <ButtonOvalImg>Learn More</ButtonOvalImg>
+            <div className="flex justify-center lg:justify-start">
+              <ButtonOvalImg>Learn More</ButtonOvalImg>
+            </div>
           </div>
         </div>
       </Container>
--- src/views/Promo.jsx
+++ src/views/Promo.jsx
@@ -9,27 +9,32 @@ function Promo() {
   return (
     <section id="promo">
       <Container>
-        <div className="my-24 flex justify-center items-center gap-4">
-          <CardPromo
-            imageUrl={woman}
-            title="UPTO 20% OFF"
-            subtitle="Collected From Gardens"
-            bgColor="green"
-            text="white"
-          />
-          <CardPromo
-            imageUrl={sayur}
-            title="UPTO 20% OFF"
-            subtitle="Change Your Diet Plan"
-            bgColor="greenlight"
-            text="black"
-          />
+        <div className="my-24 flex flex-col sm:flex-row justify-center items-center gap-4">
+        <div className="flex flex-row sm:flex-row gap-4 w-full sm:w-auto">
+            <CardPromo
+              imageUrl={woman}
+              title="UPTO 20% OFF"
+              subtitle="Collected From Gardens"
+              bgColor="green"
+              text="white"
+              hideImageOnMobile={true}
+            />
+            <CardPromo
+              imageUrl={sayur}
+              title="UPTO 20% OFF"
+              subtitle="Change Your Diet Plan"
+              bgColor="greenlight"
+              text="black"
+              hideImageOnMobile={true}
+            />
+          </div>
           <CardPromo
             imageUrl={juice}
             title="NATURAL FREASH"
             subtitle="Vegetable 100% Organic"
             bgColor="orange"
             text="white"
+            hideImageOnMobile={false}
           />
         </div>
       </Container>
