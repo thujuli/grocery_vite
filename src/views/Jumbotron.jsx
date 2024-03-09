11c4811a17600a451848ca679faa79b8a642734c
I Putu Julianta <95893131+thujuli@users.noreply.github.com>
2024-03-09 10:39:48 +0700

    feat: Responsive design section jumbotron for mobile (#40)

--- src/views/Jumbotron.jsx
+++ src/views/Jumbotron.jsx
@@ -9,21 +9,21 @@ function Jumbotron() {
       <Container>
         <div
           style={{ backgroundImage: `url(${background})` }}
-          className="flex items-center min-h-screen w-full px-[50px] bg-cover bg-center"
+          className="flex items-center min-h-screen w-full px-0 md:px-[50px] bg-cover bg-center"
         >
-          <div className="w-1/2">
+          <div className="w-full md:w-1/2 px-4 md:px-0">
             <h2
               style={{ fontFamily: "Roboto" }}
               className="text-xl text-[#3cb815] font-medium"
             >
               WE ARE DELICACY
             </h2>
-            <h1 className="mb-10 text-7xl font-bold leading-tight text-[#1a2428]">
+            <h1 className="mt-2 mb-10 text-5xl md:text-7xl font-bold leading-tight text-[#1a2428]">
               Choose delicacy the best healthy way to life
             </h1>
             <ButtonOvalImg>Shop now</ButtonOvalImg>
           </div>
-          <div className="w-1/2">
+          <div className="hidden md:block w-1/2">
             <img src={imgJumbotron} alt="Image Jumbotron" />
           </div>
         </div>
